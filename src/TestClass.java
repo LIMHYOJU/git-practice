40c8ddda83ba66e2f3a00a4b7a7aee8005a12ebb
HyojuLim <dlagywn94@gmail.com>
2022-08-01 22:33:11 +0900

    delete class modified class

--- src/TestClass.java
+++ src/TestClass.java
@@ -6,8 +6,8 @@ public class TestClass {
         TestClass3 test3 = new TestClass3();
         test3.viewText();
         test3.viewName();
-        TestClass4 test4 = new TestClass4();
-        test4.viewText();
+        TestClass5 test5 = new TestClass5();
+        test5.viewText2();
 	}
 
 }
--- src/TestClass4.java
+++
@@ -1,6 +0,0 @@
-
-public class TestClass4 {
-	public void viewText() {
-		System.out.println("hello class4");
-	}
-}
