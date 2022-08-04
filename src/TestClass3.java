aaac1c08e6f7cbe407b712a3d3b8064e49075092
HyojuLim <dlagywn94@gmail.com>
2022-08-04 16:10:30 +0900

    Merge branch 'main' of https://github.com/LIMHYOJU/git-practice
    # Conflicts:
    #	src/TestClass5.java

--- src/TestClass3.java
+++ src/TestClass3.java
@@ -1,6 +1,6 @@
 
 public class TestClass3 {
-	int a=100;
+	int a=1001;
 	public void viewText() {
 		System.out.println("\"hello!\"");
 	}
--- src/TestClass6.java
+++ src/TestClass6.java
@@ -1,10 +1,6 @@
 
 public class TestClass6 {
-<<<<<<< HEAD
-	int c = 1;
-=======
 	int c = 4001;
->>>>>>> 3c62f70 (edit TestClass6)
 	public void viewText() {
 		System.out.println("hello hyoju");
 	}
